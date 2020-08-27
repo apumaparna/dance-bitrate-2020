80139a501b47165bbf6c2d62582a949229633866
Glitch (dance-project) <none>
2020-08-27 22:39:49 +0000

    changed the size of the canvas and the dancer screen

--- dance-reader.js
+++ dance-reader.js
@@ -15,8 +15,10 @@ let go = true;
 let callback = function(results) {
   if (go) {
     poses = results;
-    console.log(poses);
+    // console.log(poses);
+    poseList.push(poses);
   }
+  console.log(poseList);
 };
 
 function setup() {
@@ -53,4 +55,67 @@ function vidLoad() {
   });
 }
 
+// Now that we have the array, let's draw it out
 
+// A function to draw ellipses over the detected keypoints
+function drawKeypoints() {
+  console.log("drawKeypoints");
+  // Loop through all the poses detected
+  for (let h = 0; h < poseList.length; h++) {
+    let frame = poseList[h];
+    // background(0);
+    for (let i = 0; i < frame.length; i++) {
+      // For each pose detected, loop through all the keypoints
+      let pose = frame[i].pose;
+      console.log(pose);
+      for (let j = 0; j < pose.keypoints.length; j++) {
+        // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+        let keypoint = pose.keypoints[j];
+        // Only draw an ellipse is the pose probability is bigger than 0.2
+        if (keypoint.score > 0.2) {
+          setTimeout( () => {
+          noStroke();
+          fill(255, 0, 0);
+          ellipse(keypoint.position.x, keypoint.position.y, 10, 10); }, 10); 
+        }
+      }
+    }
+  }
+}
+
+// A function to draw the skeletons
+function drawSkeleton() {
+  console.log("drawSkeleton");
+  // Loop through all the skeletons detected
+  for (let h = 0; h < poseList.length; h++) {
+    let frame = poseList[h];
+    // background(0);
+    for (let i = 0; i < frame.length; i++) {
+      let skeleton = frame[i].skeleton;
+      // For every skeleton, loop through all body connections
+      for (let j = 0; j < skeleton.length; j++) {
+        let partA = skeleton[j][0];
+        let partB = skeleton[j][1];
+
+        setTimeout(() => {
+          stroke(255, 0, 0);
+          line(
+            partA.position.x,
+            partA.position.y,
+            partB.position.x,
+            partB.position.y
+          );
+        }, 10);
+      }
+    }
+  }
+}
+
+function draw() {
+  background(0);
+  if (!go) {
+    // frameRate(2)
+    setTimeout(drawKeypoints(), 1000);
+    setTimeout(drawSkeleton(), 1000);
+  }
+}
