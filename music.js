043bee4bc9315051f022f2b59983d4bd334006b6
Glitch (dance-project) <none>
2020-08-28 02:36:25 +0000

    drums and grooVAE was added

--- music.js
+++ music.js
@@ -1,4 +1,4 @@
-/* global music_rnn Tone Nexus core*/
+/* global music_rnn Tone Nexus core music_vae*/
 
 // Tone.Transport.bpm.value = 220;
 
@@ -58,6 +58,44 @@ function key() {
   console.log(songKey + "7");
 }
 
+// drum instruments
+let players = new Tone.Players({
+  kick: "https://teropa.info/ext-assets/drumkit/kick.mp3",
+  hatClosed: "https://teropa.info/ext-assets/drumkit/hatClosed.mp3",
+  hatOpen: "https://teropa.info/ext-assets/drumkit/hatOpen2.mp3",
+  snare: "https://teropa.info/ext-assets/drumkit/snare3.mp3",
+  tomLow: "https://teropa.info/ext-assets/drumkit/tomLow.mp3",
+  tomMid: "https://teropa.info/ext-assets/drumkit/tomMid.mp3",
+  tomHigh: "https://teropa.info/ext-assets/drumkit/tomHigh.mp3",
+  ride: "https://teropa.info/ext-assets/drumkit/ride.mp3",
+  crash: "https://teropa.info/ext-assets/drumkit/hatOpen.mp3"
+}).toDestination();
+
+let midiToDrum = new Map([
+  [36, "kick"],
+  [37, "snare"],
+  [38, "snare"],
+  [40, "snare"],
+  [42, "hatClosed"],
+  [22, "hatClosed"],
+  [44, "hatClosed"],
+  [46, "hatOpen"],
+  [26, "hatOpen"],
+  [43, "tomLow"],
+  [58, "tomLow"],
+  [47, "tomMid"],
+  [45, "tomMid"],
+  [50, "tomHigh"],
+  [48, "tomHigh"],
+  [49, "crash"],
+  [52, "crash"],
+  [55, "crash"],
+  [57, "crash"],
+  [51, "ride"],
+  [53, "ride"],
+  [59, "ride"]
+]);
+let drumToMidi = new Map([...midiToDrum].map(e => e.reverse()));
 
 // create an instrument for the melody
 let leadSampler = new Tone.Sampler({
@@ -75,12 +113,35 @@ let leadPattern = [];
 let numNotes = leadPattern.length;
 
 let leadPart = new Tone.Part((time, note) => {
-  console.log(note);
   leadSampler.triggerAttackRelease(note, "2n", time);
 }, leadPattern).start();
 leadPart.loop = true;
 leadPart.loopStart = 0;
 leadPart.loopEnd = "2m";
+
+let drumBeat = [
+  ["0:0:0", "kick"],
+  ["0:1:0", "hatClosed"],
+  ["0:1:2", "kick"],
+  ["0:2:0", "kick"],
+  ["0:3:0", "hatClosed"],
+  ["1:0:0", "kick"],
+  ["1:1:0", "hatClosed"],
+  ["1:2:0", "kick"],
+  ["1:2:3", "kick"],
+  ["1:3:0", "hatClosed"],
+  ["1:3:2", "kick"],
+  ["1:3:2", "hatOpen"]
+];
+
+let drumPart = new Tone.Part((time, drum) => {
+  players.player(drum).start(time);
+  console.log("started sound");
+}, drumBeat).start();
+// have to manually loop for parts
+drumPart.loop = true;
+drumPart.loopStart = 0;
+drumPart.loopEnd = "2m";
 
 Tone.Transport.scheduleRepeat(time => {
   // use the callback time to schedule events
@@ -158,3 +219,41 @@ async function generateMusic() {
 
   console.log(noteNames);
 }
+
+
+let grooVae = new music_vae.MusicVAE(
+  "https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/groovae_2bar_humanize"
+);
+let grooVaeLoaded = grooVae.initialize();
+
+async function grooveDrums() {
+  await grooVaeLoaded;
+
+  let sixteenthNoteTicks = Tone.Time("16n").toTicks();
+  let original = {
+    notes: drumBeat.map(([time, drum]) => ({
+      pitch: drumToMidi.get(drum),
+      quantizedStartStep: Tone.Time(time).toTicks() / sixteenthNoteTicks,
+      quantizedEndStep: Tone.Time(time).toTicks() / sixteenthNoteTicks + 1
+    })),
+    totalQuantizedSteps: 32,
+    quantizationInfo: { stepsPerQuarter: 4 }
+  };
+
+  let z = await grooVae.encode([original]);
+  let result = await grooVae.decode(
+    z,
+    1.4,
+    undefined,
+    4,
+    Tone.Transport.bpm.value
+  );
+
+  drumPart.clear();
+  for (let note of result[0].notes) {
+    drumPart.at(note.startTime, midiToDrum.get(note.pitch));
+  }
+
+  console.log(result);
+};
+
